267b29c8d8a9a8b8290af8c68d6ab3eb47858e83
Ubuntu <ubuntu@ip-172-31-27-193.us-west-2.compute.internal>
2013-07-12 16:32:34 +0000

    fixed typo

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var msg = fs.readFileSnc('index.html');
+  var msg = fs.readFileSync('index.html');
   response.send(msg.toString());
 });
 
